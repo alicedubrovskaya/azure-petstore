4a41e3ef008db50929e55ce51857c8aedfe0fcc7
Anton Lytunenko <antonlit@gmail.com>
2022-12-15 19:03:25 +0200

    Fix issue with regestry deployment
    - use suffix to configure container registry name
    - enable CI for App Service

--- modules/module3/deployment/deploy-app-service.sh
+++ modules/module3/deployment/deploy-app-service.sh
@@ -4,14 +4,15 @@ set -e
 
 appServiceName=$1
 dockerImageTag=$2
-settings=$3
+appSuffix=$3
+settings=$4
 appServicePlanName="${appServiceName}plan"
 source $(dirname "$0")/parameters.sh
 
-dockerAcrUri="${containerRegistryName}.azurecr.io/${dockerImageTag}"
+dockerAcrUri="${containerRegistryName}${appSuffix}.azurecr.io/${dockerImageTag}"
 
 echo "Pushing image to registry: ${dockerAcrUri}"
-az acr login -n $containerRegistryName
+az acr login -n "${containerRegistryName}${appSuffix}"
 docker tag "$dockerImageTag" "$dockerAcrUri"
 docker push "$dockerAcrUri"
 
@@ -20,7 +21,8 @@ az appservice plan create --name "$appServicePlanName" --resource-group "$rgName
 
 echo "Creating App Service Web App.: $appServiceName"
 az webapp create -g "$rgName" -p "$appServicePlanName" -n "$appServiceName" -i "$dockerAcrUri"
-
+# Enable continuous deployment
+az webapp deployment container config --enable-cd true -g "$rgName" -n "$appServiceName"
 if [ -n "$settings" ]
 then
   echo "Configure Settings"
--- modules/module3/deployment/deploy-web-and-api.sh
+++ modules/module3/deployment/deploy-web-and-api.sh
@@ -13,7 +13,7 @@ webAppTag=petstoreapp
 webAppSubdomain="${webAppTag}${appSuffix}"
 
 #creating RG and ACR first to let container ACR time to initialize, while dockers are being built
-./deploy-rg-acr.sh
+./deploy-rg-acr.sh $appSuffix
 
 echo "Building PetService"
 docker build -t $petserviceTag ../../../petstore/petstorepetservice
@@ -28,7 +28,7 @@ echo "Building Web App"
 docker build -t $webAppTag ../../../petstore/petstoreapp
 
 # back to working dir
-./deploy-app-service.sh $petserviceSubdomain $petserviceTag
-./deploy-app-service.sh $productserviceSubdomain $productserviceTag
-./deploy-app-service.sh $orderserviceSubdomain $orderserviceTag "PETSTOREPRODUCTSERVICE_URL=https://${productserviceSubdomain}.azurewebsites.net/"
-./deploy-app-service.sh $webAppSubdomain $webAppTag "PETSTOREPRODUCTSERVICE_URL=https://${productserviceSubdomain}.azurewebsites.net PETSTOREPETSERVICE_URL=https://${petserviceSubdomain}.azurewebsites.net PETSTOREORDERSERVICE_URL=https://${orderserviceSubdomain}.azurewebsites.net"
+./deploy-app-service.sh $petserviceSubdomain $petserviceTag $appSuffix
+./deploy-app-service.sh $productserviceSubdomain $productserviceTag $appSuffix
+./deploy-app-service.sh $orderserviceSubdomain $orderserviceTag $appSuffix "PETSTOREPRODUCTSERVICE_URL=https://${productserviceSubdomain}.azurewebsites.net/"
+./deploy-app-service.sh $webAppSubdomain $webAppTag $appSuffix "PETSTOREPRODUCTSERVICE_URL=https://${productserviceSubdomain}.azurewebsites.net PETSTOREPETSERVICE_URL=https://${petserviceSubdomain}.azurewebsites.net PETSTOREORDERSERVICE_URL=https://${orderserviceSubdomain}.azurewebsites.net"
